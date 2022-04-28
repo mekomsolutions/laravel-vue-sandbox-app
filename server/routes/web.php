268b6ec1455da0927a400452f06948d42b2201de
Ian <ian@mekomsolutions.com>
2022-04-28 14:50:45 -0400

    Revert "(temp): dump request headers instead of serving a page"
    This reverts commit febd6bf138682d31a26028117cbfb68356fde59b.

--- server/routes/web.php
+++ server/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return apache_request_headers();
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
